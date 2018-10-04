6ebf3064c70a6459e6e17477661b15aee5cc009e
Arfan Uddin <arfan.rfn@gmail.com>
2018-10-03 20:10:52 -0700

    update app.js with intro data_processing class

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const test = require("./model/test");
+const DataProcessingObj = require('./model/data_processing');
 
 const app = express();
+
+var dataProcessing = new DataProcessingObj((err) => {
+    console.log(err.message);
+});
 
 const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    dataProcessing.findAUser({username: 'rfn'}, (err, result)=>{
+        if(err){
+            res.end("something went wrong");
+        }else if(result){
+            res.json(result);
+        }else{
+            res.send('sorry no data found');
+        }
+    });
 });
 
 app.listen(server_port, server_ip, () => {
